e471f66f43cedda9778b4bfeb979b1045b44565e
nilsreich <nilsreich@gmail.com>
2023-01-24 11:35:35 +0000

    pwa test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	let url;
-	const getFeed = async (feed) => {
+	let url:string;
+	const getFeed = async (feed:string) => {
 		const response = await fetch(`/api/add`, {
 			method: 'POST',
 			headers: {
@@ -12,15 +12,15 @@
 		url = await data.match(/<enclosure url="(.*)" length/)[1];
 	};
 </script>
-
-<div class="flex flex-col h-screen">
+<div class="fixed z-10 w-full">
 	<div class="flex justify-evenly mx-auto">
-		<button class="btn btn-ghost w-1/3" on:click={() => getFeed('https://www.tagesschau.de/export/video-podcast/webm/tagesschau_https/')} >Tagesschau</button>
-		<button class="btn btn-ghost w-1/3" on:click={() => getFeed('https://www.zdf.de/rss/podcast/video/zdf/kinder/logo')}>Logo</button>
-		<button class="btn btn-ghost w-1/3" on:click={() => getFeed('https://mediathekviewweb.de/feed?query=Aktuelles%20journal%20junior')} >Arte</button>
+		<button class="btn dark:text-white btn-ghost w-1/3" on:click={() => getFeed('https://www.tagesschau.de/export/video-podcast/webm/tagesschau_https/')} >Tagesschau</button>
+		<button class="btn dark:text-white btn-ghost w-1/3" on:click={() => getFeed('https://www.zdf.de/rss/podcast/video/zdf/kinder/logo')}>Logo</button>
+		<button class="btn dark:text-white btn-ghost w-1/3" on:click={() => getFeed('https://mediathekviewweb.de/feed?query=Aktuelles%20journal%20junior')} >Arte</button>
 		</div>
-		<video src={url} class="w-full m-auto" controls />
-
-</div>
+	</div>
+		<video src={url} autoplay class="h-screen  m-auto" controls >
+			<track kind="captions" />
+		</video>
 
 
